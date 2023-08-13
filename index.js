00566f4a4d7fa5493e20103479683c5c89ff3cb0
Bayan Elian Alreqeb <104948334+se-bayan-elian@users.noreply.github.com>
2023-08-13 11:11:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
 
-const PORT = 9000
+const PORT = 9000 || process.env.PORT
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`)
